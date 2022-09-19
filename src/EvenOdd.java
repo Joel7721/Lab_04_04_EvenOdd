126b380cb5eafd10ced99393ec4dc09ce624e82a
Joel7721 <mensahjl@mail.uc.edu>
2022-09-18 21:29:35 -0400

    Completed Even & Odd program.

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        int number = 1999;
+        int value;
+
+        value = number % 2;
+
+        System.out.println("The number being used is: " + number);
+        System.out.println("If the value shown is 0, then the number is even. If the value shown is 1, then the number is odd: " + value);
+
+    }
+}
